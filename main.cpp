fc5d6defdee7077beb438479a7eef699c883a0ed
Deivis Pereira <dcpereira@inf.ufsm.br>
2018-03-11 22:52:40 -0300

    shits happens, i don't remember how uses matrix pointer =/

--- main.cpp
+++ main.cpp
@@ -9,8 +9,12 @@ int min(int top, int left, int dia){
   return min(min(top, left), dia);
 }
 //endregion
+//Encaminha a matriz e a posição cartesiana da célula em questão
+int compare(int **matriz, int i, int j){
+  return min(matriz[i - 1][j -1], matriz[i - 1][j],matriz[i][j -1]);
+}
 void edtDistance(std::string* s0, std::string* s1){
-  //region Create and pre-Populatte M A T R I X
+  //region Cria e pre-Popula M A T R I X
   int rows = (*s0).size() + 1, cols = (*s1).size() + 1;
   int **matriz{nullptr};
   matriz = new int*[rows];
@@ -21,6 +25,20 @@ void edtDistance(std::string* s0, std::string* s1){
   for(auto i = 0; i < cols; i++)
     matriz[0][i] = i;
   //endregion
+  //region Logic
+  int i, j, t, lim = rows + cols -2;
+  for(t = 1; t <= lim -1; ++t){
+    //Slices
+    for(i = t, j = 1; i >= 1 ; --i, ++j)
+      if((i < rows) && (j < cols)){
+        std::cout << i << ", " << j << " | ";
+        matriz[i][j] = compare(matriz, i, j);
+      }
+    std::cout << std::endl;
+  }
+  //endregion
+
+  //std::cout << matriz[rows-1][cols-1] << std::endl;
 }
 
 int main() {
