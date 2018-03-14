9c97b1401ba9db6a723023940961ce182e59bcea
Deivis Pereira <dcpereira@inf.ufsm.br>
2018-03-14 00:28:34 -0300

    FakeMatrix doesn't work

---
+++ main_fakeMatrix.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+//region M-in(ax)'s
+int min(int a, int b){
+  return (a < b) ? a : b;
+}
+int min(int top, int left, int dia){
+  return min(min(top, left), dia);
+}
+int max(int a, int b){
+  return (a > b) ? a : b;
+}
+//endregion
+
+void edtDistance2(std::string* s0, std::string* s1){
+  int s0len = (*s0).size(), s1len = (*s1).size();
+  int sMax = max(s0len, s1len) + 1, sMin = min(s0len, s1len);
+  //region Create Diagonals
+  int * dAnterior2 {new int[sMax]};
+  dAnterior2[0] = 0;               // 0 - 1 - X
+  int * dAnterior {new int[sMax]}; // 1 - X - x
+  dAnterior[0] = 1;                // X - x - x
+  dAnterior[1] = 1;
+  int * dAtual {new int[sMax]};
+  int * dTemp {new int[sMax]};
+  //endregion
+  // sl = Slices/Fatias/Diagonal_Atual
+  for(int sl = 2, istart, iend; sl <= s0len + s1len; sl++){
+    std::cout << std::endl;
+    istart = max(1, sl - s1len);
+    iend = min(sl - 1, s0len);
+    // Fill Diagonals with base, if need
+    if(sl <= sMin){
+      dAtual[0] = sl;
+      dAtual[sl] = sl;
+    }
+    // Real Joke is Here =/
+    for(int i = istart, j; i <= iend; i++){
+        j = sl - i;
+        dAtual[i] = min(dAnterior[i], dAnterior[i-1], dAnterior2[i - 1]) + ((*s0)[j - 1] == (*s1)[i - 1] ? 0 : 1);
+        std::cout << dAtual[i] << std::endl;
+    }
+
+    dAnterior2 = dAnterior;
+    dAnterior = dAtual;
+    dAtual = dTemp;
+  }
+  std::cout << dAnterior[(*s1).size()] << std::endl;
+}
+
+int main() {
+  std::string s0, s1;
+  std::cin >> s0;
+  std::cin >> s1;
+  if(s0.size() < s1.size())
+    s0.swap(s1);
+  edtDistance2(&s0, &s1);
+  return 0;
+}
